cdc5bce298a2acde9147ca6811e79007550af4c4
Jing Zhang <zhangjing@microsoft.com>
2025-03-19 17:47:11 +0000

    UT

--- misc/tests/utils_unittest.cpp
+++ misc/tests/utils_unittest.cpp
@@ -41,11 +41,10 @@ TEST(Utils, ValidUrlConversion)
         {"sonic/dash.tunnel.Tunnel", "DASH_TUNNEL_TABLE"},
         {"sonic/dash.vnet_mapping.VnetMapping", "DASH_VNET_MAPPING_TABLE"},
         {"sonic/dash.vnet.Vnet", "DASH_VNET_TABLE"},
-        {"sonic/dash.dpu_ha_scope.HaScope", "DASH_HA_SCOPE_TABLE"},
-        {"sonic/dash.dpu_ha_set.HaSet", "DASH_HA_SET_TABLE"},
-        {"sonic/dash.npu_ha_set.HaSetConfig", "DASH_HA_SET_CONFIG_TABLE"},
-        {"sonic/dash.npu_ha_scope.HaScopeConfig", "DASH_HA_SCOPE_CONFIG_TABLE"},
-
+        {"sonic/dash.ha_scope.HaScope", "DASH_HA_SCOPE_TABLE"},
+        {"sonic/dash.ha_set.HaSet", "DASH_HA_SET_TABLE"},
+        {"sonic/dash.ha_set_config.HaSetConfig", "DASH_HA_SET_CONFIG_TABLE"},
+        {"sonic/dash.ha_scope_config.HaScopeConfig", "DASH_HA_SCOPE_CONFIG_TABLE"},
     };
     set<string> ignore_files = {
         "types.proto"
